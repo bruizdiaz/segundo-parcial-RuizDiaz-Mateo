d55a4e2befd785e8267613edec93d36ade62f12f
bruizdiaz <mateord65@gmail.com>
2025-06-27 10:03:29 -0300

    Configuracion de las rutas para Movies y comienzo de los controladores.

---
+++ src/controllers/movie.controllers.js
@@ -0,0 +1,7 @@
+import { Movie } from '../models/movie.model';
+
+export const getAllMovies = async (req, res) => {};
+export const getMovieByID = async (req, res) => {};
+export const createMovie = async (req, res) => {};
+export const updateMovie = async (req, res) => {};
+export const deleteMovie = async (req, res) => {};
--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,1 +1,18 @@
 import { Router } from 'express';
+import {
+	getAllMovies,
+	getMovieByID,
+	createMovie,
+	updateMovie,
+	deleteMovie,
+} from '../controllers/movie.controllers';
+
+const router = Router();
+
+router.get('/', getAllMovies);
+router.get('/:id', getMovieByID);
+router.post('/', createMovie);
+router.put('/:id', updateMovie);
+router.delete('/:id', deleteMovie);
+
+export default router;
